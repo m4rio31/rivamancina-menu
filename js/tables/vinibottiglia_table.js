f63d4e8abe3d7b0a4781e7de70265ff85004e2fa
Mario Lamberti <62987179+m4rio31@users.noreply.github.com>
2022-04-07 13:20:33 +0200

    Update vinibottiglia_table.js

--- js/tables/vinibottiglia_table.js
+++ js/tables/vinibottiglia_table.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded",()=>{
   let data=window.viniBottigliaData;
   let table = document.getElementById("viniBottigliaTable")
-  let innerHtml='<th class="th_nome">VINI IN BOTTIGLIA</th>'
+  let innerHtml='<th class="th_nome">VINI</th><th class="th_prezzo">In Bottiglia</th>'
   for(let i=0;i<data.length;i++){
     innerHtml+="<tr>"
     innerHtml+=`<td class="td_nome"><div class="td_up">${data[i].Nome}</div><div class="td_down">${data[i].Descrizione}</div></td>`    
