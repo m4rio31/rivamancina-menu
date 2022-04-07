00aa68865346e21fe720c0838d3d72f7bf367b9d
Mario Lamberti <62987179+m4rio31@users.noreply.github.com>
2022-04-07 12:47:37 +0200

    Create analcolici_table.js

---
+++ js/tables/analcolici_table.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",()=>{
+  let data=window.rossiData;
+  let table = document.getElementById("analcoliciTable")
+  let innerHtml='<th class="th_nome">Analcolici</th>'
+  for(let i=0;i<data.length;i++){
+    innerHtml+="<tr>"
+    innerHtml+=`<td class="td_nome"><div class="td_up">${data[i].Nome}</div><div class="td_down">${data[i].Descrizione}</div></td>`    
+    innerHtml+=`<td class="td_prezzo">${data[i].Prezzo}</td>`
+    innerHtml+="</tr>"
+  }
+  table.innerHTML=innerHtml
+})
