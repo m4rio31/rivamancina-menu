cee5e8d23d7358a776fe165ab49c55c90174825b
rivamancina <93984963+rivamancina@users.noreply.github.com>
2022-04-11 12:49:07 +0200

    BIANCHI

--- js/input/bianchi.js
+++ js/input/bianchi.js
@@ -2,17 +2,17 @@
 window.bianchiData = [
   {
     "Nome": "CUSTOZA D.O.C.",
-    "Descrizione":"100% TURBIANA - (VR)",
+    "Descrizione":"100% TURBIANA - MENEGOTTI(VR)",
     "Prezzo": "2€",
   },
   {
-    "Nome": "FALANGHINA",
-    "Descrizione":"100% MELON DE BOURGOGNE - FRANCIA",
+    "Nome": "FALANGHINA DEL SANNIO D.O.C.",
+    "Descrizione":"100% FALANGHINA- AIA DEI COLOMBI(BN)",
     "Prezzo": "3.5€",
   },
   {
-    "Nome": "GRILLO",
-    "Descrizione":"100% MELON DE BOURGOGNE - FRANCIA",
+    "Nome": "GRILLO SICILIA I.G.P.",
+    "Descrizione":"100% GRILLO - CANTINE FINA (TP)",
     "Prezzo": "3.5€",
   },
 ];
