29f6d9151b75ac2b7968cfb1342a8ad50eb7219c
rivamancina <93984963+rivamancina@users.noreply.github.com>
2022-04-11 12:41:47 +0200

    C

--- js/input/vinisfusi.js
+++ js/input/vinisfusi.js
@@ -9,5 +9,8 @@ window.viniSfusiData = [
   {
     "Nome": " ROSSO - CANTINA CARUSSIN (AT)",
     "Descrizione": " ",
+    "Bicchiere": "Bicchiere 2€",
+    "MezzoLitro": "1/2 litro 7€",
+    "Litro": "1 litro 12€",
   },
 ];
