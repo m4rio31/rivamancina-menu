3c60b1dfa171635f4120950e984c9e44f342520e
rivamancina <93984963+rivamancina@users.noreply.github.com>
2022-04-11 13:35:04 +0200

    ROSSI

--- js/input/rossi.js
+++ js/input/rossi.js
@@ -1,18 +1,19 @@
 
 window.rossiData = [
   {
+    "Nome": "MONTEPULCIANO D.O.C.",
+    "Descrizione": "100% MONTEPULCIANO - CANTINA TOLLO (CH)",
+    "Prezzo": "3.5€",
+  },
+  {
     "Nome": "VALPOLICELLA RIPASSO D.O.C.",
     "Descrizione": "15% CORVINA 40% CORVINONE 40% RONDINELLA 5% MOLINARA - SPADA (VR)",
     "Prezzo": "3.5€",
   },
+  
   {
-    "Nome": "MONTEPULCIANO D.O.C.",
-    "Descrizione": "90% SANGIOVESE 10% CILIEGIOLO - AZ.AGR. LE CALLE (BR)",
-    "Prezzo": "3.5€",
-  },
-  {
-    "Nome": "AGLIANICO DEL SANNIO D.O.C.",
-    "Descrizione": "100% AGLIANICO - AIA DEI COLOMBI (BN)",
+    "Nome": "BARBERA D'ALBA D.O.C.",
+    "Descrizione": "100% BARBERA - VAL DEL PRETE (CN)",
     "Prezzo": "3.5€",
   },
 ];
