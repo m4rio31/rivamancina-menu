8603b42dedc241b9df2596cecdc3aeced1b40326
rivamancina <93984963+rivamancina@users.noreply.github.com>
2022-05-06 17:01:54 +0200

    BIRRA

--- js/input/birraspina.js
+++ js/input/birraspina.js
@@ -7,9 +7,15 @@ window.birraSpinaData = [
     "Media": "Media 4€",
   },
   {
-    "Nome": "PASHA' BIRRIFICIO FARNESE- I.P.A. ARTIGIANALE",
+    "Nome": "LA PAZ- A.P.A. ARTIGIANALE",
     "Descrizione": "",
     "Piccola": "Piccola 2.5€",
     "Media": "Media 5€",
+  }, 
+  {
+    "Nome": "SINERGIA '21 DUBBEL 7,5%- TRAPPISTA ARTIGIANALE ITALIANA",
+    "Descrizione": "TRE FONTANE (ROMA)",
+    "Piccola": "0,25L 4€",
+    ,
   },
 ];
